4867568b89555fe704a5fd3b827b046ae8f2d464
Vidyu8 <150137627+Vidyu8@users.noreply.github.com>
2024-11-09 11:21:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import History from './pages/History';
+import Regions from './pages/Regions';
+import Ethnicities from './pages/Ethnicities';
+import Myths from './pages/Myths';
+import Quizzes from './pages/Quizzes';
+import Quiz1 from './pages/Quiz1';
+import Quiz2 from './pages/Quiz2';
+import Quiz3 from './pages/Quiz3';
+import Quiz4 from './pages/Quiz4';
+import DarkMoments from './pages/DarkMoments';
+import Mystery from './pages/Mysteries';
+import Mytho from './pages/Mytho';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/history" element={<History />} />
+                <Route path="/regions" element={<Regions />} />
+                <Route path="/ethnicities" element={<Ethnicities />} />
+                <Route path="/myths" element={<Myths />} />
+                <Route path="/quizzes" element={<Quizzes />} />
+                <Route path="/quiz1" element={<Quiz1 />} />
+                <Route path="/quiz2" element={<Quiz2 />} />
+                <Route path="/quiz3" element={<Quiz3 />} />
+                <Route path="/quiz4" element={<Quiz4 />} />
+                <Route path="/darkhistory" element={<Darkhistory />} />
+                <Route path="/mystery" element={<Mystery />} />
+                <Route path="/mytho" element={<Mythology />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer/>
+        </Router>
+    );
 }
 
 export default App;
